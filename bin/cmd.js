98d2d38eb450c6673971f70fb64a19f44318640c
Dmitry <df.creative@gmail.com>
2014-11-26 17:21:49 +0300

    Add basic options

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,22 +3,65 @@ var fs = require('fs');
 var uc = require('../');
 var path = require('path');
 
+var version = '0.1.x';
+
 process.stdout.on('error', process.exit);
 
 
 //get options
 //TODO: extend options
 var opts = require('nomnom')
-// .option('debug', {
-// 	abbr: 'd',
-// 	flag: true,
-// 	help: 'Print debugging info'
-// })
+.option('version', {
+	abbr: 'v',
+	help: 'Show version of uncommon.',
+	flag: true,
+	callback: function(){
+		var pkg = require('../package.json');
+		console.log(pkg.name + ' ' + pkg.version);
+		return process.exit(1);
+	}
+})
+.option('debug', {
+	abbr: 'd',
+	flag: true,
+	help: 'Provide source maps in output.'
+})
 .option('basedir', {
 	abbr: 'b',
 	flag: false,
-	help: 'Setup base dir to resolve modules'
+	metavar: 'PATH',
+	help: 'Setup base dir to resolve modules.'
 })
+.option('require', {
+	list: true,
+	abbr: 'r',
+	metavar: 'MODULE[:ALIAS]',
+	help: 'Provide a global `require`. Optionally set a module alias via colon.',
+	flag: false
+})
+.option('standalone', {
+	abbr: 's',
+	metavar: 'NAME',
+	help: 'Generate an UMD bundle for the supplied export name.'
+})
+.option('ignore', {
+	abbr: 'i',
+	metavar: 'MODULE',
+	help: 'Don’t include supplied modules/files to bundle.',
+	flag: false,
+	list: true
+})
+.option('comments', {
+	abbr: 'c',
+	help: 'Keep modules comments in export.',
+	flag: true,
+	default: true
+})
+// .option('wrapper', {
+// 	abbr: 'w',
+// 	metavar: 'VAL',
+// 	help: 'Wrap the result '
+// })
 .parse();
 
 
--- test/index.js
+++ test/index.js
@@ -8,8 +8,63 @@
 var uncommon = require('../index');
 
 
-describe('Build', function(){
-	it('1 file no args build', function(){
+describe('Options', function(){
+	it.skip('uncommon', function(){
 		uncommon();
 	});
+
+	//include require fn
+	it.skip('uncommon -r module -r sub/module', function(){
+
+	});
+
+	//provide global var with proper name
+	it.skip('uncommon -s module', function(){
+
+	});
+
+	//provide specific names for exported modules
+	it.skip('uncommon -r module:x -r sub/module:y ', function(){
+
+	});
+
+	//ignore module to include (provide global getter)
+	it.skip('uncommon -i', function(){
+
+	});
+
+	//keep comments in exported file
+	it.skip('uncommon -c', function(){
+
+	});
+
+	//provide source maps for includes
+	it.skip('uncommon -d', function(){
+
+	});
+
+	//default output package wrapper (no wrap by default)
+	it.skip('uncommon -w=...', function(){
+
+	});
+
+	//show help
+	it.skip('uncommon -h', function(){
+
+	});
+
+	//show version of uncommon
+	it.skip('uncommon -v', function(){
+
+	});
+
+	//basedir for input stream (default is currDir)
+	it.skip('uncommon -b', function(){
+
+	});
+});
+
+
+describe('Edge cases', function(){
+
 });
